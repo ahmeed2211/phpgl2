7478ddab701b76530823ddf774c52aaa51ed3ebe
ahmeed221 <ahmedskhiri13@gmail.com>
2025-04-04 23:12:22 +0100

    added class

--- Ex2/SessionManager.php
+++ Ex2/SessionManager.php
@@ -1,4 +1,24 @@
 <?php
 class SessionManager{
+    private $nbvisites;
+    public function __construct(){
+        session_start();
+        if (isset($_SESSION['nbvisites'])) {
+            $this->nbvisites = $_SESSION['nbvisites'] + 1;
+        } else {
+            $this->nbvisites = 1;
+        }
+        $_SESSION['nbvisites'] = $this->nbvisites;
+    }
+    public function incrementer(){
+        $this->nbvisites++;
+        $_SESSION['nbvisites'] = $this->nbvisites;
+    }
+    public function getNbVisites(){
+        return $this->nbvisites;
+    }
+    public function reset(){
+        $_SESSION['nbvisites'] = 0;
+    }
 }
 ?>
