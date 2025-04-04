2bcb3e97821deb8af848fab4a679e729876f8529
HoussemChourou <houssam.chourou@insat.usar.tn>
2025-04-04 23:56:31 +0100

    first attack

--- Ex3/Pokemon.php
+++ Ex3/Pokemon.php
@@ -1,5 +1,57 @@
 <?php
+require_once 'AttackPokemon.php';
 class Pokemon
 {
+    public function __construct(protected string $name="",
+    protected string $url="",
+    protected int $hp=0,
+    protected AttackPokemon $attackPokemon){
 
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+    public function getHp(){
+        return $this->hp;
+    }
+    public function getAttackPokemon(){
+        return $this->attackPokemon;
+    }
+    public function setHp($hp){
+        $this->hp = $hp;
+    }
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function setUrl($url){
+        $this->url = $url;
+    }
+    public function setAttackPokemon($attackPokemon){
+        $this->attackPokemon=$attackPokemon;
+    }
+
+    public function isDead(){
+        return $this->hp <= 0;
+    }
+
+    public function attack(Pokemon $p){
+        $atk=mt_rand($this->attackPokemon->attackMinimal,$this->attackPokemon->attackMaximal);
+        $special=mt_rand(1,100)<=$this->attackPokemon->probabilitySpecialAttack;
+        if($special){
+            $atk*=$this->attackPokemon->specialAttack;
+        }
+        echo $atk;
+        $p->setHp($p->getHp()-$atk);
+        return $p;
+    }
+    public function whoAmI(){
+        echo $this->name;
+        echo $this->hp;
+        echo $this->url;
+        echo $this->attackPokemon;
+    }
 }
---
+++ Ex3/index.php
@@ -0,0 +1,58 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once 'Pokemon.php';
+require_once 'AttackPokemon.php';
+$attacks=[
+    new AttackPokemon(10,100,2,20),
+    new AttackPokemon(30,80,4,20)
+];
+
+$pokemons = [
+    new Pokemon("Pikachu", "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png", 200, $attacks[1]),
+    new Pokemon("Charizard", "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/006.png", 200, $attacks[0])
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon Battle</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body class="p-4">
+<p class="alert alert-primary" role="alert">Les combattants</p>
+<div class="d-flex flex-wrap gap-4">
+    <?php foreach ($pokemons as $pokemon) : ?>
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+            <?php echo $pokemon->getName(); ?>
+            <img src=<?= $pokemon->getUrl(); ?> class="card-img-top" alt="pokemon">
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Points: <?= $pokemon->getHp() ?> </li>
+            <li class="list-group-item">Min Attack Points: <?= $pokemon->getAttackPokemon()->attackMinimal ?></li>
+            <li class="list-group-item">Max Attack Points: <?= $pokemon->getAttackPokemon()->attackMaximal ?></li>
+            <li class="list-group-item">special Attack: <?= $pokemon->getAttackPokemon()->specialAttack ?></li>
+            <li class="list-group-item">Probability Special Attack: <?= $pokemon->getAttackPokemon()->probabilitySpecialAttack ?></li>
+        </ul>
+    </div>
+    <?php endforeach; ?>
+    <div class="alert alert-danger" role="alert">
+        Round 1
+        <div class="alert alert-light" role="alert">
+            <?php
+                $pokemons[1]=$pokemons[0]->attack($pokemons[1]);
+                $pokemons[0]=$pokemons[1]->attack($pokemons[0]);
+            ?>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
