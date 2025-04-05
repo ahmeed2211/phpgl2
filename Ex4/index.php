596473a5eeacfe0a792cdb46e9432e5487be9158
ahmeed221 <ahmedskhiri13@gmail.com>
2025-04-05 16:04:57 +0100

    base de donnees tpphp

---
+++ Ex4/index.php
@@ -0,0 +1,46 @@
+<?php 
+try{
+    $base= new PDO("mysql:host=localhost;dbname=tpphp","root",'password');
+    echo "connection successful";
+    $query = $base->query("SELECT * FROM Student");
+    $students = $query->fetchAll(PDO::FETCH_ASSOC);
+}
+catch(PDOException $e){
+    echo $e->getMessage();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students</title>
+</head>
+<body>
+<div class="container mt-4">
+        <h1>liste des Etudiants</h1>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>ID  </th>
+                    <th>Nom </th>
+                    <th>Date de Naissance  </th>
+                    <th>details</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($students as $student) {
+                    echo "<tr>";
+                    echo "<td>" .$student['id']. "</td>";
+                    echo "<td>" .$student['name'] . "</td>";
+                    echo "<td>" .$student['datenaissance'] . "</td>";
+                    echo "<td><a href='detailEtudiant.php?id=" .$student['id']. "'>Details</a></td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
