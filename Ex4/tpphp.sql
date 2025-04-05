69368f62faeffe95857e1691723f9c219bf8690f
ahmeed221 <ahmedskhiri13@gmail.com>
2025-04-05 15:56:41 +0100

    base de donnees tpphp

---
+++ Ex4/tpphp.sql
@@ -0,0 +1,8 @@
+Create database tpphp;
+create table tpphp.Student (
+id int primary key,
+name varchar(20),
+datenaissance date);
+
+INSERT into tpphp.Student values(1,"ahmed","2004-11-22");
+insert into tpphp.Student values(2,"Houssem","2004-05-10");
